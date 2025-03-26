8df689b14bc132393c428d740d86c36a1a14d67f
Carabalone <guilherme.caraba@gmail.com>
2025-03-26 15:55:45 -0300

    fix: syntax error that wasn't caught in compilation

--- src/IBM/IBMUtils.cuh
+++ src/IBM/IBMUtils.cuh
@@ -1,11 +1,12 @@
 #ifndef IBMUTILS_H
 #define IBMUTILS_H
+#include "core/lbm_constants.cuh"
 
 __host__ __device__
-float delta(r) {
+float delta(float r) {
     return fabsf(r) <= 1 ? 
             1.0f - r 
-            : 0
+            : 0;
 }
 
 __host__ __device__ __forceinline__
@@ -19,21 +20,4 @@ float distance(float x1, float y1, float x2, float y2) {
     float dy = y1 - y2;
     return sqrtf(dx*dx + dy*dy);
 }
-
-__host__ __device__ __forceinline__
-void getAffectedNodeRange(float x, float y, int& x_start, int& y_start, int& x_end, int& y_end) {
-    x_start = max(0, (int)floorf(x - 2.0f));
-    y_start = max(0, (int)floorf(y - 2.0f));
-    x_end = min(NX-1, (int)ceilf(x + 2.0f));
-    y_end = min(NY-1, (int)ceilf(y + 2.0f));
-}
-
-// Determine if a lattice node is near a boundary point
-__host__ __device__ __forceinline__
-bool isNodeNearBoundary(float x_boundary, float y_boundary, int x_node, int y_node) {
-    float dx = fabsf(x_boundary - x_node);
-    float dy = fabsf(y_boundary - y_node);
-    return (dx <= 2.0f && dy <= 2.0f);
-}
-
 #endif // ! IBMUTILS_H
